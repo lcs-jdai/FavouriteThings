5ff661c255d2a86002ab9c6dabb2a0ca8f028752
lcs-jdai <jdai@lcs.on.ca>
2021-10-25 11:47:01 -0400

    Rearranged Files For better organizations.

---
+++ Viewssss/YuangeView.swift
@@ -0,0 +1,33 @@
+//
+//  YuangeView.swift
+//  FavouriteThings
+//
+//  Created by Jerry Dai on 2021-10-25.
+//
+
+import SwiftUI
+
+struct YuangeView: View {
+    var body: some View {
+        
+        VStack {
+            
+            Image("yuange")
+                .resizable()
+                .scaledToFit()
+            
+            Text("Yuange YYDS")
+            
+                .padding()
+            
+                Spacer()
+        }
+        .navigationTitle("Yuange")
+    }
+}
+
+struct YuangeView_Previews: PreviewProvider {
+    static var previews: some View {
+        YuangeView()
+    }
+}
