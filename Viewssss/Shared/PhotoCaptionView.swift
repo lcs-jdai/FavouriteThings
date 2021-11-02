8ed9d5f152e3cd580a06a3d52107ca2eceac6967
lcs-jdai <jdai@lcs.on.ca>
2021-11-02 14:48:21 -0400

    created photocaptionview

---
+++ Viewssss/Shared/PhotoCaptionView.swift
@@ -0,0 +1,51 @@
+//
+//  PhotoCaptionView.swift
+//  FavouriteThings
+//
+//  Created by Jerry Dai on 2021-11-02.
+//
+
+import SwiftUI
+
+struct PhotoCaptionView: View {
+    var body: some View {
+        
+        ScrollView {
+        
+        VStack {
+            
+            VStack (alignment: .leading){
+                
+                Text ("Seraphine")
+                    .fontWeight(.black)
+                
+                Image("seraphine")
+                    .resizable()
+                    .scaledToFit()
+                
+                VStack (alignment: .leading) {
+                    Text ("Introduction")
+                    
+                    Text ("photo credit: foodom")
+                    
+                }
+                .padding(.vertical)
+            }
+           
+            
+            
+            
+            Text("Seraphine Born in Piltover to Zaunite parents, Seraphine can hear the souls of others—the world sings to her, and she sings back. Though these sounds overwhelmed her in her youth, she now draws on them for inspiration, turning the chaos into a symphony. She performs for the sister cities to remind their citizens that they’re not alone, that they’re stronger together, and that, in her eyes, their potential is limitless.")
+
+            }
+        .padding()
+        }
+        .navigationTitle("Seraphine")
+    }
+}
+
+struct PhotoCaptionView_Previews: PreviewProvider {
+    static var previews: some View {
+        PhotoCaptionView()
+    }
+}
