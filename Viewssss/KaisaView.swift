2d17a523907a78409c99ba397732402a61859831
lcs-jdai <jdai@lcs.on.ca>
2021-11-02 14:37:46 -0400

    Did a little change to the seraphim's page.

--- Viewssss/KaisaView.swift
+++ Viewssss/KaisaView.swift
@@ -19,11 +19,11 @@ struct KaisaView: View {
                 .scaledToFit()
 
             Text("Kaisa is a burst marksman and usually played in the bottom lane. In lore, Kai'Sa is rumored to be the daughter of another champion, Kassadin, who was lost to the Void, an alternate plane of existence connected to Runeterra filled with many unspeakable horrors amidst the utter nothingness and darkness. To survive, she merged with a Voidborn, a creature of the Void, becoming an apex predator herself and has since returned to Runeterra not fully Voidborn, but remarkably less human.")
+                
             
             .padding()
-                
-            Spacer ()
 
+            Spacer()
             }
         }
         .navigationTitle("Kaisa")
--- Viewssss/SeraphineView.swift
+++ Viewssss/SeraphineView.swift
@@ -14,17 +14,31 @@ struct SeraphineView: View {
         
         VStack {
             
-            Image("seraphine")
-                .resizable()
-                .scaledToFit()
+            VStack (alignment: .leading){
+                
+                Text ("Seraphine")
+                    .fontWeight(.black)
+                
+                Image("seraphine")
+                    .resizable()
+                    .scaledToFit()
+                
+                VStack (alignment: .leading) {
+                    Text ("Introduction")
+                    
+                    Text ("photo credit: foodom")
+                    
+                }
+                .padding(.vertical)
+            }
+           
+            
+            
             
             Text("Seraphine Born in Piltover to Zaunite parents, Seraphine can hear the souls of others—the world sings to her, and she sings back. Though these sounds overwhelmed her in her youth, she now draws on them for inspiration, turning the chaos into a symphony. She performs for the sister cities to remind their citizens that they’re not alone, that they’re stronger together, and that, in her eyes, their potential is limitless.")
-            
-                .padding()
-            
-                Spacer()
-        
+
             }
+        .padding()
         }
         .navigationTitle("Seraphine")
     }
